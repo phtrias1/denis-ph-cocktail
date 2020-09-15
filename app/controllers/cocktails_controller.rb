8ba76319908fa9c0855d85d12c0a40e7c064194a
pedrotrias <pedropedro0225@gmail.com>
2020-09-15 14:48:28 -0300

    show page routed

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,14 +1,28 @@
 class CocktailsController < ApplicationController
 
   def index 
-      @cocktails = Cocktail.all
+    @cocktails = Cocktail.all
   end
-
+  
   def show 
-    @restaurant = Restaurant.find(params[:id])      
+    @cocktail = Cocktail.find(params[:id])      
   end
 
   def new 
-    @cocktail = Cocktail.create
+    @cocktail = Cocktail.new
   end
+  
+  def create 
+    @cocktail = Cocktail.new(cocktail_params)
+    @cocktail.save 
+
+    redirect_to cocktails_path(@cocktail)
+  end
+  
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
   get "cocktails", to: "cocktails#index"
-  get "restaurants/:id", to: "restaurants#show", as: :restaurant
+  get "cocktails/new", to: "cocktails#new" 
+  post "cocktails", to: "cocktails#create"
+
+  get "cocktails/:id", to: "cocktails#show", as: :cocktail
+
 
  
 end
